66de911f3b6e00a529d3b7268b12760a4562f49a
giannis <johntriantos13@hotmail.com>
2018-05-14 20:14:19 +0300

    Create rotateCounterClockwise and fix code in rotateClockwise

--- src/Cube.java
+++ src/Cube.java
@@ -24,6 +24,42 @@ public class Cube {
 	public void rotateClockwise(int row) {
 		if (row==1) {
 			for (int i=0; i<3; i++) {
+				Color[] rowOne = new Color[COLUMNS];
+				Color[] rowTwo = new Color[COLUMNS];
+				Color[] rowThree = new Color[COLUMNS];
+				rowOne[i] = cube[0][i][4];
+				rowTwo[i] = cube[1][i][4];
+				rowThree[i] = cube[2][i][4];
+				cube[i][2][4] = rowOne[i];
+				cube[i][1][4] = rowTwo[i];
+				cube[i][0][4] = rowThree[i];
+			}
+			rotateRow(0,row);
+		} else if (row==2) {
+			rotateRow(0,row);
+		} else if (row==3) {
+			for (int i=0; i<3; i++) {
+				Color[] rowOne = new Color[COLUMNS];
+				Color[] rowTwo = new Color[COLUMNS];
+				Color[] rowThree = new Color[COLUMNS];
+				rowOne[i] = cube[0][i][5];
+				rowTwo[i] = cube[1][i][5];
+				rowThree[i] = cube[2][i][5];
+				cube[i][2][5] = rowOne[i];
+				cube[i][1][5] = rowTwo[i];
+				cube[i][0][5] = rowThree[i];
+			}
+			rotateRow(0,row);
+		}
+	}
+	
+	/**
+	 * Rotates a front side's row counterclockwise
+	 * @param row The row of the cube to rotate
+	 */
+	public void rotateCounterClockwise(int row) {
+		if (row==1) {
+			for (int i=0; i<3; i++) {
 				Color[] colOne = new Color[ROWS];
 				Color[] colTwo = new Color[ROWS];
 				Color[] colThree = new Color[ROWS];
@@ -34,9 +70,9 @@ public class Cube {
 				cube[1][i][4] = colTwo[i];
 				cube[2][i][4] = colOne[i];
 			}
-			rotateRow(0,row);
+			rotateRow(1,row);
 		} else if (row==2) {
-			rotateRow(0,row);
+			rotateRow(1,row);
 		} else if (row==3) {
 			for (int i=0; i<3; i++) {
 				Color[] colOne = new Color[ROWS];
@@ -49,16 +85,8 @@ public class Cube {
 				cube[1][i][5] = colTwo[i];
 				cube[2][i][5] = colOne[i];
 			}
-			rotateRow(0,row);
+			rotateRow(1,row);
 		}
-	}
-	
-	/**
-	 * Rotates a front side's row counterclockwise
-	 * @param row The row of the cube to rotate
-	 */
-	public void rotateCounterClockwise(int row) {
-		
 	}
 	
 	/**
