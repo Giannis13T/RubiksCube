de5e5516cbfbdba80e96a43884c69f3b0a9fcee6
giannis <johntriantos13@hotmail.com>
2018-06-16 03:15:35 +0300

    Create visited method

--- src/FindBest.java
+++ src/FindBest.java
@@ -2,6 +2,9 @@ import java.awt.Color;
 import java.util.ArrayList;
 
 public class FindBest {
+	
+	private final int ROWS = 3;
+	private final int COLUMNS = 3;
 
 	ArrayList<State> states = new ArrayList<State>();
 	ArrayList<State> visited = new ArrayList<State>();
@@ -14,6 +17,32 @@ public class FindBest {
 	 */
 	public FindBest(Color[][][] c) {
 		cube = new Cube(c);
+	}
+	
+	/**
+	 * Checks if a specific state has been produced again
+	 * @param st The state to be checked
+	 */
+	public boolean visited(State st) {
+		boolean found = true;
+		Color[][][] c = st.getCube();
+		for (State s: visited) {
+			Color[][][] cb = s.getCube();
+			for (int i=0; i<6; i++) {
+				for (int j=0; j<ROWS; j++) {
+					for (int k=0; k<COLUMNS; k++) {
+						if (c[j][k][i]!=cb[j][k][i]) {
+							found = false;
+							break;
+						} else found = true;
+					}
+					if (!found) break;
+				}
+				if (!found) break;
+			}
+			if (found) break;
+		}
+		return found;
 	}
 	
 }
